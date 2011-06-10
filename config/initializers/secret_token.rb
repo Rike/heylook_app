f84626f7d0b7eda2cfaa717817b5d1cc831c7426
Friederike Schmiedebach <f.schmiedebach@gmx.de>
2011-06-10 21:57:57 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heylook::Application.config.secret_token = '02539ebe67de96e173d424a8a913b1e638a5267358e7b3029fc176ee3d51455b41aeb5c71f19899d4fe5fc49d867be14e2b67686fe3c3bcb006fa20114f576fc'
