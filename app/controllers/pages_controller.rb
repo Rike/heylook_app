a08d3c48f0072ca3a77e62e0db9f90833bca6b76
Friederike Schmiedebach <f.schmiedebach@gmx.de>
2011-06-13 00:31:22 +0200

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
+  
   def corporate_events
+    @title = "Corporate events"
   end
 
   def sales_spaces
+    @title = "Sales spaces"
   end
 
   def weddings
+    @title = "Weddings"
   end
-
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
+  
   describe "GET 'corporate_events'" do
     it "should be successful" do
       get 'corporate_events'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+          get 'corporate_events'
+          response.should have_selector("title",
+                            :content => "Hey Look | Corporate events")
     end
   end
 
@@ -14,6 +21,12 @@ describe PagesController do
       get 'sales_spaces'
       response.should be_success
     end
+    
+    it "should have the right title" do
+          get 'sales_spaces'
+          response.should have_selector("title",
+                            :content => "Hey Look | Sales spaces")
+    end
   end
 
   describe "GET 'weddings'" do
@@ -21,6 +34,11 @@ describe PagesController do
       get 'weddings'
       response.should be_success
     end
+    
+    it "should have the right title" do
+          get 'weddings'
+          response.should have_selector("title",
+                            :content => "Hey Look| Weddings")
+    end
   end
-
 end
